98ca822597a65b94ba0b83e26d5e136eddc00b26
WaltonBaker <loganceng@loganceng@gmail.com>
2021-06-25 23:22:33 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+export default router
